3efc5cdeb49506dc71b93e89ee81f49921ad9dd2
Lakshaya Sachdeva <lakshaya.sachdeva@colearn.id>
2023-10-18 13:43:06 +0530

    Initial Commit

---
+++ DoubleParallaxSlider/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DoubleParallaxSlider
+//
+//  Created by Lakshaya Sachdeva on 18/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DoubleParallaxSlider/DoubleParallaxSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoubleParallaxSliderApp.swift
+//  DoubleParallaxSlider
+//
+//  Created by Lakshaya Sachdeva on 18/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DoubleParallaxSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
